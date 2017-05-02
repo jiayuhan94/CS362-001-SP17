c564d9632771cf3863d53e8fed80b9673eede7a3
jiayuhan94 <hanjia@oregonstate.edu>
2017-05-01 19:24:51 -0700

    ApptTest finished with 100% coverage

--- projects/hanjia/assignment-2/Calendar/src/test/java/edu/osu/cs362/ApptTest.java
+++ projects/hanjia/assignment-2/Calendar/src/test/java/edu/osu/cs362/ApptTest.java
@@ -39,6 +39,127 @@ public class ApptTest {
 		 assertEquals("This is my birthday party.", appt.getDescription());         		
 	 }
 
+	 @Test
+	public void setStartHourTest(){
+		 int startHour = 25;
+		 int startMinute=30;
+		 int startDay=10;
+		 int startMonth=4;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data
+		 Appt appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 title,
+				 description);
+		 // assertions
+		 assertFalse(appt.getValid());
+		 assertEquals(25, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(10, appt.getStartDay());
+		 assertEquals(04, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());
+	 }
 
-	
+	@Test
+	public void setStartHourTest1(){
+		int startHour=13;
+		int startMinute=60;
+		int startDay=10;
+		int startMonth=4;
+		int startYear=2017;
+		String title= null;
+		String description= null;
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		// assertions
+		assertFalse(appt.getValid());
+		assertEquals(13, appt.getStartHour());
+		assertEquals(60, appt.getStartMinute());
+		assertEquals(10, appt.getStartDay());
+		assertEquals(04, appt.getStartMonth());
+		assertEquals(2017, appt.getStartYear());
+		assertEquals("", appt.getTitle());
+		assertEquals("", appt.getDescription());
+	}
+
+	@Test
+	public void toStringtest(){
+		int startHour=13;
+		int startMinute=30;
+		int startDay=10;
+		int startMonth=4;
+		int startYear=2017;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		appt.toString();
+
+		assertNotEquals(null,appt.toString());
+	}
+
+	@Test
+	public void toStringtest1(){
+		int startHour=25;
+		int startMinute=30;
+		int startDay=10;
+		int startMonth=4;
+		int startYear=2017;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		appt.toString();
+
+		assertEquals(null,appt.toString());
+	}
+
+
+	@Test
+	public void toStringtest2(){
+		int startHour=12;
+		int startMinute=30;
+		int startDay=10;
+		int startMonth=4;
+		int startYear=2017;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		appt.toString();
+
+		assertNotEquals(null,appt.toString());
+	}
+
 }
